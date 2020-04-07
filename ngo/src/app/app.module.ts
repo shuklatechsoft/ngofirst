eac93687ca2a6e6060a702700a6e34e167d2d9f3
shukla456shivam <62144320+shukla456shivam@users.noreply.github.com>
2020-04-07 17:14:02 +0530

    inital commit

---
+++ ngo/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './common/header/header.component';
+import { FooterComponent } from './common/footer/footer.component';
+import { MainComponent } from './main/main.component';
+import { VolunterComponent } from './volunter/volunter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    MainComponent,
+    VolunterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ngo/src/app/volunter/volunter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-volunter',
+  templateUrl: './volunter.component.html',
+  styleUrls: ['./volunter.component.css']
+})
+export class VolunterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
